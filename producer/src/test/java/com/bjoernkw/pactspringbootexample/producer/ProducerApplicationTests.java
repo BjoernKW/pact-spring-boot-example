bf6473e4190382829eabd44bfaec52959a6e8087
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-12-13 17:37:45 +0100

    Generated sample applications

---
+++ producer/src/test/java/com/bjoernkw/pactspringbootexample/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjoernkw.pactspringbootexample.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
