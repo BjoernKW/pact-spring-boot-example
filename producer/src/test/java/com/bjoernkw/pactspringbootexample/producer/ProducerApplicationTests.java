8a1d05c07c5039424ca81f0bf67ddc5aec812e46
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-12-15 20:29:22 +0100

    Testcontainers setup for LocalStack

--- producer/src/test/java/com/bjoernkw/pactspringbootexample/producer/ProducerApplicationTests.java
+++ producer/src/test/java/com/bjoernkw/pactspringbootexample/producer/ProducerApplicationTests.java
@@ -2,9 +2,22 @@ package com.bjoernkw.pactspringbootexample.producer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SNS;
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SQS;
 
 @SpringBootTest
+@Testcontainers
 class ProducerApplicationTests {
+
+	@Container
+	static LocalStackContainer localStack =
+			new LocalStackContainer(DockerImageName.parse("localstack/localstack"))
+					.withServices(SQS, SNS);
 
 	@Test
 	void contextLoads() {
