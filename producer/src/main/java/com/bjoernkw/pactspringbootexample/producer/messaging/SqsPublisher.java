4d477202bf8ba7c5b8c39212a68cb0773b966cb2
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-12-15 21:01:54 +0100

    Testcontainers setup for LocalStack

--- producer/src/main/java/com/bjoernkw/pactspringbootexample/producer/messaging/SqsPublisher.java
+++ producer/src/main/java/com/bjoernkw/pactspringbootexample/producer/messaging/SqsPublisher.java
@@ -3,11 +3,9 @@ package com.bjoernkw.pactspringbootexample.producer.messaging;
 import io.awspring.cloud.messaging.core.QueueMessagingTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("worker")
 public class SqsPublisher {
 
     @Autowired
