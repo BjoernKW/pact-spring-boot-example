3df175494e4da8d8b891a9b24ac526fe2712d7f7
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-12-16 01:52:09 +0100

    Testcontainers setup for LocalStack

--- consumer/src/test/java/com/bjoernkw/pactspringbootexample/consumer/ConsumerApplicationTests.java
+++ consumer/src/test/java/com/bjoernkw/pactspringbootexample/consumer/ConsumerApplicationTests.java
@@ -1,56 +1,24 @@
 package com.bjoernkw.pactspringbootexample.consumer;
 
-import com.amazonaws.services.sqs.AmazonSQSAsync;
-import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.localstack.LocalStackContainer;
-import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
-import java.io.IOException;
-
-import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SQS;
+import java.io.File;
 
 @SpringBootTest
-@Testcontainers
+@Testcontainers(disabledWithoutDocker = true)
 class ConsumerApplicationTests {
 
-	private static final String QUEUE_NAME = "test-queue";
+	public static DockerComposeContainer<?> environment =
+			new DockerComposeContainer<>(new File("../infrastructure/docker-compose.yml"))
+					.withExposedService("localstack", 4566, Wait.forListeningPort())
+					.withLocalCompose(true);
 
-	@Container
-	static LocalStackContainer localStack =
-			new LocalStackContainer(DockerImageName.parse("localstack/localstack"))
-					.withServices(SQS);
-
-	@TestConfiguration
-	static class AWSTestConfiguration {
-
-		@Bean
-		public AmazonSQSAsync amazonSQS() {
-			return AmazonSQSAsyncClientBuilder.standard()
-					.withEndpointConfiguration(localStack.getEndpointConfiguration(SQS))
-					.withCredentials(localStack.getDefaultCredentialsProvider())
-					.build();
-		}
-	}
-
-	@BeforeAll
-	static void beforeAll() throws IOException, InterruptedException {
-		localStack.execInContainer("awslocal", "sqs", "create-queue", "--queue-name", QUEUE_NAME);
-	}
-
-	@DynamicPropertySource
-	static void overrideConfiguration(DynamicPropertyRegistry registry) {
-		registry.add("sqs.queue", () -> QUEUE_NAME);
-		registry.add("cloud.aws.credentials.access-key", localStack::getAccessKey);
-		registry.add("cloud.aws.credentials.secret-key", localStack::getSecretKey);
+	static {
+		environment.start();
 	}
 
 	@Test
